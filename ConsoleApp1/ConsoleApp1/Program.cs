d786b768c37be854f279e780951ed521066405d9
ROHIT BADGUJAR <badgujarrohit7@gmail.com>
2024-10-07 11:07:59 -0500

    multithreading task

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AutoResetDemo
+    {
+        static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread th = new Thread(SomeMethod);
+                th.Start();
+            }
+            Console.ReadLine();
+            autoResetEvent.Set();
+            //implement Reset()
+        }
+        static void SomeMethod()
+        {
+            Console.WriteLine("Starting........");
+            autoResetEvent.WaitOne();
+            Console.WriteLine("Finishing..........");
+            Console.ReadLine();
+        }
+    }
+}
